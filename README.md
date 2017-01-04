406a301f1b8811f3d6cfdeb327e704667b6236d3
Janis <janis.k@wpbakery.com>
2017-01-04 22:21:51 +0200

    Loading add, columns and gap add

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # masonry-gallery
-This is masonry gallery plugin
+This is very simple masonry gallery plugin
+
+Example:
+
+`<div class="masonry-gallery" data-masonry-gallery-columns="3" data-masonry-gallery-gap="8">
+     <img src="" alt="">
+     <img src="" alt="">
+     <img src="" alt="">
+ </div>`
+
+
+
+
--- js/masonry.js
+++ js/masonry.js
@@ -24,35 +24,43 @@
         // delete images
         masonry.innerHTML = '';
 
+        masonry.style.marginBottom = 0 - gap + 'px';
+
         columns = parseInt(columns);
         columns <= 0 ? columns = 1 : '';
 
         for (var i = 0; i < columns; i++) {
             var column = document.createElement('div');
             column.className = 'masonry-gallery-column';
-            masonry.appendChild(column)
+            column.style.flexBasis = 'calc(100% /' + columns + ')';
+            column.style.maxWidth = 'calc(100% /' + columns + ')';
+            if (i !== columns - 1) {
+                column.style.paddingRight = gap + 'px';
+            }
+            masonry.appendChild(column);
         }
 
         columnElements = masonry.querySelectorAll('.masonry-gallery-column');
 
+        masonry.classList.add('masonry-gallery-loaded');
         loadImages();
 
         function loadImages() {
             var img = new window.Image();
             img.src = imgSources[currentImg];
+            img.style.marginBottom = gap + 'px';
             insertImage(img);
-            img.onload = imgLoadHandler.bind(this)
+            img.onload = imgLoadHandler.bind(this, img)
         }
 
         function insertImage(imgElement) {
-            imgElement.className = 'vce-image-masonry-gallery-img';
+            imgElement.className = 'masonry-gallery-img masonry-gallery-img-loading';
             var customImageProps = {
                 'alt': 'alt text',
                 'title': 'title'
             };
             setAttributes(imgElement, customImageProps);
             var smallestColIndex = getSmallestColumn(columnElements);
-            console.log(imgElement)
             columnElements[smallestColIndex].appendChild(imgElement)
         }
 
@@ -62,7 +70,9 @@
             }
         }
 
-        function imgLoadHandler() {
+        function imgLoadHandler(imgElement) {
+            imgElement.classList.remove('masonry-gallery-img-loading');
+            imgElement.classList.add('masonry-gallery-img-loaded');
             currentImg++;
             if (currentImg < imgSources.length) {
                 loadImages()
@@ -89,6 +99,5 @@
             });
             return smallestIndex
         }
-
     })
 })();
